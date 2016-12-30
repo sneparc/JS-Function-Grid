42bce3e439ddbfe76a594a64a41ed56ca1a9a097
Casey Snepar <sneparc@gmail.com>
2016-12-30 20:03:01 +0000

    four corners

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Write your function calls below this line
+ function setColor(row, col, color) {
+    var cell = $("#t")[0].rows[row].cells[col];
+    cell.style.backgroundColor = color;
+             }
+setColor(0,0, "blue");
+setColor(9, 0, "blue");
+setColor(0, 9, "blue");
+setColor(9, 9, "blue");
